6446fd8f2a54da6703f46190d2a705c7132b3b7c
razif.malik <razif.malik@idealo.de>
2021-08-12 12:36:02 +0200

    Added country Filters

--- Queries/main.sql
+++ Queries/main.sql
@@ -2,6 +2,7 @@ select * from dl_dwh_prod.new_fact_leadins
 where date_utc = date( '2021-02-01')
 
 and mkt_channel in ('Display', 'Remarketing', 'SEM', 'Affiliate', 'CRM', 'SEO')
+and country in ('DE', 'FR')
 
 
 limit 10;
