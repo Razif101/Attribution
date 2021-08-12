d6a4d017ea91c47466ae677f80bd8ae295003199
Razif101 <81317885+Razif101@users.noreply.github.com>
2021-08-12 12:39:58 +0200

    Update main.sql

--- Queries/main.sql
+++ Queries/main.sql
@@ -2,7 +2,7 @@ select * from dl_dwh_prod.new_fact_leadins
 where date_utc = date( '2021-02-01')
 
 and mkt_channel in ('Display', 'Remarketing', 'SEM', 'Affiliate', 'CRM', 'SEO')
-and country in ('DE', 'FR')
+and country in ('DE', 'FR', 'IT')
 
 
 limit 10;
